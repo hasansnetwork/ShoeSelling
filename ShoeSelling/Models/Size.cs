df5ebd6822c998eea86e49534855288560a2365f
hasan <hasansnetwork@github.com>
2019-11-26 02:06:26 +0000

    Hasan Aydogmus Size Model

---
+++ ShoeSelling/Models/Size.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoeSelling.Models
+{
+    public class Size
+    {
+
+        [Key]
+        [Display(Name = "Sıze ID")]
+        public int SizeId { get; set; }
+        [Display(Name = "Number")]
+        public int Number { get; set; }
+
+
+    }
+}
